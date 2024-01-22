96729391963a3afd95a3509d6e8d64a034203508
raghav-1998 <raghav.vasdeva@gmail.com>
2024-01-23 00:54:06 +0530

    Creating Score Count Section for Quiz App

---
+++ src/components/ScoreCount.js
@@ -0,0 +1,9 @@
+function ScoreCount({score}){
+    return (
+        <div className="alert alert-info scoreCount" role="alert">
+            Score : {score}
+        </div>
+    );
+}
+
+export default ScoreCount;
